60e9d7dfc66067d93117f1513e2ffa6f7732664c
Ahmet Taha Kılıç <ahmettahaklc@gmail.com>
2026-07-19 22:55:36 +0300

    Fix undefined variable names in db.php PDO connection
    DB connection used $db_name/$username/$password which were never
    defined; the loaded env vars are $db/$user/$pass. Also ignore *.sql
    dumps to avoid committing real user data.

--- backend/api/db.php
+++ backend/api/db.php
@@ -18,7 +18,7 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-W
 
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
    
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
